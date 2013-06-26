324093a77eca067a90cfd4158b2f3db8b85b3f59
naoya <mine@mine-studio.com>
2013-06-27 00:38:26 +0900

    Format code

--- 4.c
+++ 4.c
@@ -14,7 +14,7 @@ double fix(double val) {
 
 int main() {
     double a, b, c, d, e, f, x, y;
-    while(scanf("%lf %lf %lf %lf %lf %lf", &a, &b, &c, &d, &e, &f) != EOF) {
+    while (scanf("%lf %lf %lf %lf %lf %lf", &a, &b, &c, &d, &e, &f) != EOF) {
         x = _round((c * e - b * f)/(a * e - b * d));
         y = _round((a * f - d * c)/(a * e - b * d));
         printf("%.3f %.3f\n", fix(x), fix(y));
